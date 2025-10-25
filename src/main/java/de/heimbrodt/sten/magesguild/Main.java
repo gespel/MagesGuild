2484c52638a3f5fccfc0be7ba187b3c72f5fa3f3
gespel <heimbrodt@uni-potsdam.de>
2025-10-25 23:35:01 +0200

    working demo

--- src/main/java/de/heimbrodt/sten/magesguild/Main.java
+++ src/main/java/de/heimbrodt/sten/magesguild/Main.java
@@ -1,11 +1,32 @@
 package de.heimbrodt.sten.magesguild;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements CommandExecutor {
     @Override
     public void onEnable() {
         getLogger().info("MagesGuild Plugin Enabled");
+        getCommand("mage").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+        String cmdName = cmd.getName();
+        System.out.println(cmdName);
+
+        if (sender instanceof Player player) {
+            if (cmdName.equalsIgnoreCase("mage")) {
+                player.sendMessage("Enabling Mage Mode...");
+            }
+
+        }
+
+        return true;
     }
 
 }
