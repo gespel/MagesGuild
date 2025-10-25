ee5b6dea545f2bd0fa09f27e441317398ba56c36
gespel <heimbrodt@uni-potsdam.de>
2025-10-25 22:56:00 +0200

    init

---
+++ src/main/java/de/heimbrodt/sten/magesguild/Main.java
@@ -0,0 +1,11 @@
+package de.heimbrodt.sten.magesguild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("MagesGuild Plugin Enabled");
+    }
+
+}
