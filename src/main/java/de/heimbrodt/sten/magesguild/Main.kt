aa7aa537c71d1f334f270c5ff4e3777a09535821
sheimbrodt <sheimbrodt@psi.de>
2025-10-27 12:26:15 +0100

    adding directory for spell definition in plugins dir

--- src/main/java/de/heimbrodt/sten/magesguild/Main.kt
+++ src/main/java/de/heimbrodt/sten/magesguild/Main.kt
@@ -7,9 +7,17 @@ import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.exists
 
 class Main : JavaPlugin(), CommandExecutor {
     override fun onEnable() {
+        if (!Path("magesguild").exists()) {
+            logger.info("No Mages Guild Path exists. Creating a new instance...")
+            Path("plugins/magesguild").createDirectories()
+        }
+
         logger.info("MagesGuild Plugin Enabled")
         getCommand("mage")!!.setExecutor(this)
     }
@@ -18,16 +26,23 @@ class Main : JavaPlugin(), CommandExecutor {
         val cmdName = cmd.name
         println(cmdName)
 
-        if (sender is Player) {
-            if (cmdName.equals("mage", ignoreCase = true)) {
+        if (cmdName.equals("mage", true)) {
+            if (sender is Player) {
                 sender.sendMessage("Enabling Mage Mode...")
             }
-            if (cmdName.equals("home", ignoreCase = true)) {
+            else {
+                sender.sendMessage("Enabled Spells are:")
+            }
+        }
+        if (cmdName.equals("home", true)) {
+            if (sender is Player) {
                 sender.sendMessage("Teleporting home...")
                 sender.teleport(Location(Bukkit.getWorld("world"), 1332.0, 63.0, 3720.0))
             }
+            else {
+                sender.sendMessage("You are no player you dum dum...")
+            }
         }
-
         return true
     }
 }
