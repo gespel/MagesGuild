d3e1c81414ad6156baf9cdd09b9c09c9b332110d
sheimbrodt <sheimbrodt@psi.de>
2025-10-27 10:34:46 +0100

    moved to kotlin!!

--- src/main/java/de/heimbrodt/sten/magesguild/Main.kt
+++ src/main/java/de/heimbrodt/sten/magesguild/Main.kt
@@ -1,38 +1,33 @@
-package de.heimbrodt.sten.magesguild;
+package de.heimbrodt.sten.magesguild
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
 
-public class Main extends JavaPlugin implements CommandExecutor {
-    @Override
-    public void onEnable() {
-        getLogger().info("MagesGuild Plugin Enabled");
-        getCommand("mage").setExecutor(this);
+class Main : JavaPlugin(), CommandExecutor {
+    override fun onEnable() {
+        logger.info("MagesGuild Plugin Enabled")
+        getCommand("mage")!!.setExecutor(this)
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+    override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<String>): Boolean {
+        val cmdName = cmd.name
+        println(cmdName)
 
-        String cmdName = cmd.getName();
-        System.out.println(cmdName);
-
-        if (sender instanceof Player player) {
-            if (cmdName.equalsIgnoreCase("mage")) {
-                player.sendMessage("Enabling Mage Mode...");
+        if (sender is Player) {
+            if (cmdName.equals("mage", ignoreCase = true)) {
+                sender.sendMessage("Enabling Mage Mode...")
             }
-            if (cmdName.equalsIgnoreCase("home")) {
-                player.sendMessage("Teleporting home...");
-                player.teleport(new Location(Bukkit.getWorld("world"), 1332.0, 63.0, 3720.0));
+            if (cmdName.equals("home", ignoreCase = true)) {
+                sender.sendMessage("Teleporting home...")
+                sender.teleport(Location(Bukkit.getWorld("world"), 1332.0, 63.0, 3720.0))
             }
-
         }
 
-        return true;
+        return true
     }
-
 }
