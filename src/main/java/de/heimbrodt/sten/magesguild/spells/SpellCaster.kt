e34386f6c2abbbb8e61aad4f9c41d926feb9c5c5
gespel <heimbrodt@uni-potsdam.de>
2025-10-30 08:44:30 +0100

    adding spellcaster class to check if spell hits a target

---
+++ src/main/java/de/heimbrodt/sten/magesguild/spells/SpellCaster.kt
@@ -0,0 +1,13 @@
+package de.heimbrodt.sten.magesguild.spells
+
+import org.bukkit.entity.Player
+
+class SpellCaster {
+    constructor(spell: Spell, caster: Player) {
+
+    }
+
+    fun cast() {
+
+    }
+}
